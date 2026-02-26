3d46b7953ac08e090eec712c9885bbbfa9ef87a5
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-26 16:55:42 +0100

    Commit inicial

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "SIDSA - Misc",
+    "name": "sid_misc",
     "version": "15.0.1.0.0",
     "category": "Hidden",
     "summary": "Miscelaneos: placeholders y extensiones sin funcionalidad concreta.",
-    "author": "SIDSA / Custom",
+    "author": "oscarsidsa81",
     "license": "LGPL-3",
     "depends": ["sale", "product"],
     "data": [],
